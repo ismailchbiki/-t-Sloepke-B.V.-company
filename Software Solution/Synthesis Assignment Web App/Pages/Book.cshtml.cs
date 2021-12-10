9fa6119ac57025764ae979813114ed4aae323812
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2021-12-10 01:07:44 +0100

    Start - Web App pages design

---
+++ Software Solution/Synthesis Assignment Web App/Pages/Book.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Synthesis_Assignment_Web_App.Pages
+{
+    public class BookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
