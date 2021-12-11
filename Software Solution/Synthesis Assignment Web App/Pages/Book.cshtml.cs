d020a5a98159b51c35bd34629a37050212b6b6d6
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2021-12-11 13:04:37 +0100

    Book Page (In progress)

--- Software Solution/Synthesis Assignment Web App/Pages/Book.cshtml.cs
+++ Software Solution/Synthesis Assignment Web App/Pages/Book.cshtml.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SynthesisAssignment.Models;
 
 namespace Synthesis_Assignment_Web_App.Pages
 {
     public class BookModel : PageModel
     {
+        private InventoryAdministration manageGear = new InventoryAdministration();
+        public List<Inventory> Gear;
+
         public void OnGet()
         {
-            
+            Gear = manageGear.AllGear();
         }
     }
 }
